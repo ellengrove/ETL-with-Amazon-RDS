bf25a7c2d9990bf72ab59f8cc95dd1da7676d227
egrove <ellen.grove95@gmail.com>
2022-08-10 17:20:54 -0500

    added schema file

---
+++ level-1/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE kitchen_review_ids (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE kitchen_products (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for first data set
+CREATE TABLE kitchen_customers (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);

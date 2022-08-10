5b99edc7b9331495c3844e9cca5ae3680fbe4e47
egrove <ellen.grove95@gmail.com>
2022-08-10 18:33:48 -0500

    added schema for outdoors tables

--- level-1/schema.sql
+++ level-1/schema.sql
@@ -17,3 +17,23 @@ CREATE TABLE kitchen_customers (
   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
   customer_count INT
 );
+
+CREATE TABLE outdoors_review_ids (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE outdoors_products (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for first data set
+CREATE TABLE outdoors_customers (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);

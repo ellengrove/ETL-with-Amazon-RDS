578922d450c8f408d5b0938055b7c973c474c7d0
egrove <ellen.grove95@gmail.com>
2022-08-11 18:20:17 -0500

    fix schema

--- level-1/schema.sql
+++ level-1/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE kitchen_review_ids (
+CREATE TABLE review_id_table (
   review_id TEXT PRIMARY KEY NOT NULL,
   customer_id INTEGER,
   product_id TEXT,
@@ -7,33 +7,13 @@ CREATE TABLE kitchen_review_ids (
 );
 
 -- This table will contain only unique values
-CREATE TABLE kitchen_products (
+CREATE TABLE products (
   product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
   product_title TEXT
 );
 
 -- Customer table for first data set
-CREATE TABLE kitchen_customers (
-  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
-  customer_count INT
-);
-
-CREATE TABLE outdoors_review_ids (
-  review_id TEXT PRIMARY KEY NOT NULL,
-  customer_id INTEGER,
-  product_id TEXT,
-  product_parent INTEGER,
-  review_date DATE -- this should be in the formate yyyy-mm-dd
-);
-
--- This table will contain only unique values
-CREATE TABLE outdoors_products (
-  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-  product_title TEXT
-);
-
--- Customer table for first data set
-CREATE TABLE outdoors_customers (
+CREATE TABLE customers (
   customer_id INT PRIMARY KEY NOT NULL UNIQUE,
   customer_count INT
 );
